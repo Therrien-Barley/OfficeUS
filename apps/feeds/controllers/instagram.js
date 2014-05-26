6a75c8350976a28036ba3744aa1bd85b401e23e5
troyth <troyth@gmail.com>
2014-05-26 16:06:53 -0400

    media length limit

--- apps/feeds/controllers/instagram.js
+++ apps/feeds/controllers/instagram.js
@@ -29,7 +29,7 @@ var fetchHandler = function(err, medias, pagination, limit) {
 
 		image_array.push(medias);
 
-		if(pagination.next){
+		if(pagination.next && (medias.length < 100) ){
 			pagination.next( fetchHandler );
 		}else{
 			res.render('feeds', { title: 'OfficeUS Outposts', medias: image_array });
