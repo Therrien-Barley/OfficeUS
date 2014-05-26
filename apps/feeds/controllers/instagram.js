3905630f1278df14ba34045f7dd08ccecfd47d3a
troyth <troyth@gmail.com>
2014-05-26 16:05:28 -0400

    using fetch handler for recursive fetch calls

--- apps/feeds/controllers/instagram.js
+++ apps/feeds/controllers/instagram.js
@@ -15,21 +15,34 @@ ig.use({ client_id: instagram_secrets.client_id,
 /////// END INSTAGRAM
 
 
+var image_array = [];
+
+
+
+var fetchHandler = function(err, result, pagination, limit) {
+  // Your implementation here
+  if(err){
+		console.log('instagram.js::fetchHandler() err with msg: ' + err);
+	}else{
+		console.log('instagram.js::'.cyan + 'returned from fetch() with: \n');
+		console.dir(pagination);
+
+		image_array.push(medias);
+
+		if(pagination.next){
+			pagination.next( fetchHandler );
+		}else{
+			res.render('feeds', { title: 'OfficeUS Outposts', medias: image_array });
+		}
+	}
+};
+
+
 
 // Polls Instagram API to see if there are any updates
 // if so, stores them in the mongodb
 exports.fetch = function(res){
-	ig.tag_media_recent('architecture', function(err, medias, pagination, limit) {
-
-		if(err){
-			console.log('instagram.js::fetch() err with msg: ' + err);
-		}else{
-			console.log('instagram.js::'.cyan + 'returned from fetch() with: \n');
-			console.dir(pagination);
-
-			res.render('feeds', { title: 'OfficeUS Outposts', medias: medias });
-		}
-	});
+	ig.tag_media_recent( 'architecture', fetchHandler );
 };
 
 
