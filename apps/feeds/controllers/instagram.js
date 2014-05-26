545624a81f6da45c078b6b79ddb331416a915688
troyth <troyth@gmail.com>
2014-05-26 15:44:10 -0400

    initial instagram.js

--- apps/feeds/controllers/instagram.js
+++ apps/feeds/controllers/instagram.js
@@ -1,5 +1,15 @@
+var colors  = require('colors');
+console.log('images.js entered'.cyan);
+/////// END DEPENDENCIES
+
+
+/////// INSTAGRAM
 var ig = require('instagram-node').instagram();
 
-ig.use({ access_token: 'YOUR_ACCESS_TOKEN' });
-ig.use({ client_id: 'YOUR_CLIENT_ID',
-         client_secret: 'YOUR_CLIENT_SECRET' });
+//import Instagram secrets from json file
+var instagram_secrets = require('../secrets.json').instagram;
+
+//set up secrets for Instagram module
+ig.use({ client_id: instagram_secrets.client_id,
+    client_secret: instagram_secrets.client_secret });
+/////// END INSTAGRAM
