286b2aa273916c3031f9b6d22cdb0080f0df2f41
troyth <troyth@gmail.com>
2014-05-26 15:38:44 -0400

    comma typo

--- apps/feeds/app.js
+++ apps/feeds/app.js
@@ -32,7 +32,7 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-app.get('/instagram' routes.instagram);
+app.get('/instagram', routes.instagram);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
